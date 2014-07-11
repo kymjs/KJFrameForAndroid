4e8218fa57c49e9fd3ef08e7f38876523298a1d3
kym僵尸 <kymjs123@gmail.com>
2014-07-11 18:30:28 +0800

    更新Bitmaplibrary

--- KJLibrary/src/org/kymjs/aframe/bitmap/KJBitmap.java
+++ KJLibrary/src/org/kymjs/aframe/bitmap/KJBitmap.java
@@ -1,53 +1,114 @@
 package org.kymjs.aframe.bitmap;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.kymjs.aframe.bitmap.utils.BitmapCreate;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
+import android.view.View;
 import android.widget.ImageView;
 
 public class KJBitmap {
 
-    private static KJBitmap instance;
-
-    public static synchronized KJBitmap create(Context ctx) {
-        if (instance == null) {
-            instance = new KJBitmap(ctx.getApplicationContext());
-        }
-        return instance;
-    }
-
-    private KJBitmap(Context context) {
-        cxt = context;
-        config = new KJBitmapConfig();
-        mMemoryCache = new MemoryCache(config.memoryCacheSize);
-    }
-
-    /************************************************************************/
-    private Context cxt;
     private KJBitmapConfig config;
     private MemoryCache mMemoryCache;
+    /** 记录所有正在下载或等待下载的任务 */
+    private Set<BitmapWorkerTask> taskCollection;
 
-    public void loadBitmap(int resId, ImageView imageView) {
-        final String imageKey = String.valueOf(resId);
+    public KJBitmap(Context context) {
+        config = new KJBitmapConfig();
+        mMemoryCache = new MemoryCache(config.memoryCacheSize);
+        taskCollection = new HashSet<BitmapWorkerTask>();
+    }
+
+    public void display(View imageView, String imageUrl) {
+        final String imageKey = String.valueOf(imageView.getId());
         final Bitmap bitmap = mMemoryCache.get(imageKey);
         if (bitmap != null) {
-            imageView.setImageBitmap(bitmap);
+            if (imageView instanceof ImageView) {
+                ((ImageView) imageView).setImageBitmap(bitmap);
+            } else {
+                imageView.setBackgroundDrawable(new BitmapDrawable(bitmap));
+            }
         } else {
-            imageView.setImageBitmap(config.loadingBitmap);
-            BitmapWorkerTask task = new BitmapWorkerTask();
-            task.execute(resId);
+            if (imageView instanceof ImageView) {
+                ((ImageView) imageView).setImageBitmap(config.loadingBitmap);
+            } else {
+                imageView.setBackgroundDrawable(new BitmapDrawable(
+                        config.loadingBitmap));
+            }
+            BitmapWorkerTask task = new BitmapWorkerTask(imageView);
+            taskCollection.add(task);
+            task.execute(imageUrl);
         }
     }
 
-    class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
-        // 在后台加载图片。
+    /**
+     * 异步下载图片的任务。
+     * 
+     * @author guolin
+     */
+    class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
+        private View imageview;
+
+        public BitmapWorkerTask(View imageview) {
+            this.imageview = imageview;
+        }
+
         @Override
-        protected Bitmap doInBackground(Integer... params) {
-            final Bitmap bitmap = BitmapCreate.bitmapFromResource(
-                    cxt.getResources(), params[0], 100, 100);
-            mMemoryCache.put(String.valueOf(params[0]), bitmap);
+        protected Bitmap doInBackground(String... params) {
+            // 在后台开始下载图片
+            Bitmap bitmap = downloadBitmap(params[0]);
+            if (bitmap != null) {
+                // 图片下载完成后缓存到LrcCache中
+                mMemoryCache.put(params[0], bitmap);
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if (imageview instanceof ImageView) {
+                if (bitmap != null) {
+                    ((ImageView) imageview).setImageBitmap(bitmap);
+                }
+            }
+            taskCollection.remove(this);
+        }
+
+        /**
+         * 建立HTTP请求，并获取Bitmap对象。
+         * 
+         * @param imageUrl
+         *            图片的URL地址
+         * @return 解析后的Bitmap对象
+         */
+        private Bitmap downloadBitmap(String imageUrl) {
+            Bitmap bitmap = null;
+            HttpURLConnection con = null;
+            try {
+                URL url = new URL(imageUrl);
+                con = (HttpURLConnection) url.openConnection();
+                con.setConnectTimeout(5 * 1000);
+                con.setReadTimeout(10 * 1000);
+                con.setDoInput(true);
+                con.setDoOutput(true);
+                bitmap = BitmapCreate.bitmapFromByteArray(con.getInputStream(),
+                        null, 100, 100);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (con != null) {
+                    con.disconnect();
+                }
+            }
             return bitmap;
         }
     }
