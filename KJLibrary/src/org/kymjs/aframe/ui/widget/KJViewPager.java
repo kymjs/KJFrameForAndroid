22b89f273f2c14f5c040e64e7d7675d3e06db87f
kymjs <kymjs123@gmail.com>
2014-09-12 15:40:16 +0800

    添加用于替换系统Viewpager的控件

---
+++ KJLibrary/src/org/kymjs/aframe/ui/widget/KJViewPager.java
@@ -0,0 +1,288 @@
+/*
+ * Copyright (c) 2014, KJFrameForAndroid 张涛 (kymjs123@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kymjs.aframe.ui.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.Scroller;
+
+/**
+ * 左右滑动切换屏幕控件,用于替换系统的ViewPager，优势在于不需要每次滑动都加载视图
+ * 
+ * <b>创建时间</b> 2014-9-12
+ * 
+ * @author kymjs(kymjs123@gmail.com)
+ * @version 1.0
+ */
+public class KJViewPager extends ViewGroup {
+    private Scroller mScroller;
+    private VelocityTracker mVelocityTracker;
+    private int mCurScreen;
+    private int mDefaultScreen = 0;
+    private static final int TOUCH_STATE_REST = 0;
+    private static final int TOUCH_STATE_SCROLLING = 1;
+    private static final int SNAP_VELOCITY = 600;
+    private int mTouchState = TOUCH_STATE_REST;
+    private int mTouchSlop;
+    private float mLastMotionX;
+    private float mLastMotionY;
+    private OnViewChangeListener mOnViewChangeListener;
+
+    /**
+     * 设置是否可左右滑动
+     */
+    private boolean isScroll = true;
+
+    public KJViewPager(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public KJViewPager(Context context, AttributeSet attrs,
+            int defStyle) {
+        super(context, attrs, defStyle);
+        mScroller = new Scroller(context);
+        mCurScreen = mDefaultScreen;
+        mTouchSlop = ViewConfiguration.get(getContext())
+                .getScaledTouchSlop();
+    }
+
+    /**
+     * 对该控件里面的子控件定位
+     */
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r,
+            int b) {
+        int childLeft = 0;
+        final int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            final View childView = getChildAt(i);
+            if (childView.getVisibility() != View.GONE) {
+                final int childWidth = childView.getMeasuredWidth();
+                childView.layout(childLeft, 0,
+                        childLeft + childWidth,
+                        childView.getMeasuredHeight());
+                childLeft += childWidth;
+            }
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec,
+            int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        final int width = MeasureSpec.getSize(widthMeasureSpec);
+        // final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        // if (widthMode != MeasureSpec.EXACTLY) {
+        // throw new IllegalStateException(
+        // "ScrollLayout only canmCurScreen run at EXACTLY mode!");
+        // }
+        // final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        // if (heightMode != MeasureSpec.EXACTLY) {
+        // throw new IllegalStateException(
+        // "ScrollLayout only can run at EXACTLY mode!");
+        // }
+
+        // The children are given the same width and height as the scrollLayout
+        final int count = getChildCount();
+        for (int i = 0; i < count; i++) {
+            getChildAt(i)
+                    .measure(widthMeasureSpec, heightMeasureSpec);
+        }
+        // Log.e(TAG, "moving to screen "+mCurScreen);
+        scrollTo(mCurScreen * width, 0);
+    }
+
+    /**
+     * According to the position of current layout scroll to the destination
+     * page.
+     */
+    public void snapToDestination() {
+        final int screenWidth = getWidth();
+        final int destScreen = (getScrollX() + screenWidth / 2)
+                / screenWidth;
+        snapToScreen(destScreen);
+    }
+
+    public void snapToScreen(int whichScreen) {
+        // 是否可滑动
+        if (!isScroll) {
+            setToScreen(whichScreen);
+        } else {
+            scrollToScreen(whichScreen);
+        }
+    }
+
+    public void scrollToScreen(int whichScreen) {
+        // get the valid layout page
+        whichScreen = Math.max(0,
+                Math.min(whichScreen, getChildCount() - 1));
+        if (getScrollX() != (whichScreen * getWidth())) {
+            final int delta = whichScreen * getWidth() - getScrollX();
+            mScroller.startScroll(getScrollX(), 0, delta, 0,
+                    Math.abs(delta) * 1);// 持续滚动时间 以毫秒为单位
+            mCurScreen = whichScreen;
+            invalidate(); // Redraw the layout
+
+            if (mOnViewChangeListener != null) {
+                mOnViewChangeListener.OnViewChange(mCurScreen);
+            }
+        }
+    }
+
+    public void setToScreen(int whichScreen) {
+        whichScreen = Math.max(0,
+                Math.min(whichScreen, getChildCount() - 1));
+        mCurScreen = whichScreen;
+        scrollTo(whichScreen * getWidth(), 0);
+
+        if (mOnViewChangeListener != null) {
+            mOnViewChangeListener.OnViewChange(mCurScreen);
+        }
+    }
+
+    public int getCurScreen() {
+        return mCurScreen;
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            postInvalidate();
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!isScroll) {
+            return false;
+        }
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+        mVelocityTracker.addMovement(event);
+        final int action = event.getAction();
+        final float x = event.getX();
+        final float y = event.getY();
+        switch (action) {
+        case MotionEvent.ACTION_DOWN:
+            if (!mScroller.isFinished()) {
+                mScroller.abortAnimation();
+            }
+            mLastMotionX = x;
+            mLastMotionY = y;
+            break;
+        case MotionEvent.ACTION_MOVE:
+            int deltaX = (int) (mLastMotionX - x);
+            int deltaY = (int) (mLastMotionY - y);
+            if (Math.abs(deltaX) < 200 && Math.abs(deltaY) > 10)
+                break;
+            mLastMotionY = y;
+            mLastMotionX = x;
+            scrollBy(deltaX, 0);
+            break;
+        case MotionEvent.ACTION_UP:
+            // if (mTouchState == TOUCH_STATE_SCROLLING) {
+            final VelocityTracker velocityTracker = mVelocityTracker;
+            velocityTracker.computeCurrentVelocity(1000);
+            int velocityX = (int) velocityTracker.getXVelocity();
+            if (velocityX > SNAP_VELOCITY && mCurScreen > 0) {
+                // Fling enough to move left
+                snapToScreen(mCurScreen - 1);
+            } else if (velocityX < -SNAP_VELOCITY
+                    && mCurScreen < getChildCount() - 1) {
+                // Fling enough to move right
+                snapToScreen(mCurScreen + 1);
+            } else {
+                snapToDestination();
+            }
+            if (mVelocityTracker != null) {
+                mVelocityTracker.recycle();
+                mVelocityTracker = null;
+            }
+            mTouchState = TOUCH_STATE_REST;
+            break;
+        case MotionEvent.ACTION_CANCEL:
+            mTouchState = TOUCH_STATE_REST;
+            break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (!isScroll) {
+            return false;
+        }
+        final int action = ev.getAction();
+        if ((action == MotionEvent.ACTION_MOVE)
+                && (mTouchState != TOUCH_STATE_REST)) {
+            return true;
+        }
+        final float x = ev.getX();
+        final float y = ev.getY();
+        switch (action) {
+        case MotionEvent.ACTION_MOVE:
+            final int xDiff = (int) Math.abs(mLastMotionX - x);
+            if (xDiff > mTouchSlop) {
+                mTouchState = TOUCH_STATE_SCROLLING;
+            }
+            break;
+        case MotionEvent.ACTION_DOWN:
+            mLastMotionX = x;
+            mLastMotionY = y;
+            mTouchState = mScroller.isFinished() ? TOUCH_STATE_REST
+                    : TOUCH_STATE_SCROLLING;
+            break;
+        case MotionEvent.ACTION_CANCEL:
+        case MotionEvent.ACTION_UP:
+            mTouchState = TOUCH_STATE_REST;
+            break;
+        }
+        return mTouchState != TOUCH_STATE_REST;
+
+    }
+
+    /**
+     * 设置屏幕切换监听器
+     * 
+     * @param listener
+     */
+    public void setOnViewChangeListener(OnViewChangeListener listener) {
+        mOnViewChangeListener = listener;
+    }
+
+    /**
+     * 屏幕切换监听器
+     */
+    public interface OnViewChangeListener {
+        public void OnViewChange(int view);
+    }
+
+    /**
+     * 设置控件是否可以左右滑动
+     * 
+     * @param scroll
+     */
+    public void setCanScroll(boolean scroll) {
+        this.isScroll = scroll;
+    }
+}
