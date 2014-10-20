66c905a3fdf9e80a2ce286eb5cd09c211e61b158
kymjs <kymjs123@gmail.com>
2014-10-20 17:53:45 +0800

    Demo工程创建

---
+++ PluginExample/src/org/kymjs/aframe/plugin/example/MainActivity.java
@@ -0,0 +1,13 @@
+package org.kymjs.aframe.plugin.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
