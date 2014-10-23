2ecee7ad58eef13d4486bedb3e393c11651a61a2
kymjs <kymjs123@gmail.com>
2014-10-23 11:33:15 +0800

    版本更新V2.0

--- KJLibrary/src/org/kymjs/aframe/KJConfig.java
+++ KJLibrary/src/org/kymjs/aframe/KJConfig.java
@@ -17,7 +17,7 @@ package org.kymjs.aframe;
 
 public final class KJConfig {
 
-    public static final double VERSION = 1.21;
+    public static final double VERSION = 2.0;
 
     /** 声音改变的广播 */
     public static final String RECEIVER_MUSIC_CHANGE = KJConfig.class
