401465c34f5ae66b9d868fa73c431e90151149b5
kymjs <kymjs123@gmail.com>
2015-10-08 16:14:38 +0800

    2.25版本发布，完美兼容6.0

--- KJFrame/kjframe/src/main/java/org/kymjs/kjframe/utils/KJConfig.java
+++ KJFrame/kjframe/src/main/java/org/kymjs/kjframe/utils/KJConfig.java
@@ -21,7 +21,7 @@ package org.kymjs.kjframe.utils;
  */
 public final class KJConfig {
 
-    public static final double VERSION = 2.2491;
+    public static final double VERSION = 2.250;
 
     /** 错误处理广播 */
     public static final String RECEIVER_ERROR = KJConfig.class.getName()
