f90a23b623169e51497e98067f7ff15f3406bd59
kymjs <kymjs123@gmail.com>
2015-10-10 14:39:22 +0800

    重构基类

--- KJFrame/kjframe/src/main/java/org/kymjs/kjframe/ui/I_KJActivity.java
+++ KJFrame/kjframe/src/main/java/org/kymjs/kjframe/ui/I_KJActivity.java
@@ -19,26 +19,42 @@ import android.view.View;
 
 /**
  * KJFrameActivity接口协议，实现此接口可使用KJActivityManager堆栈<br>
- * 
+ * <p/>
  * <b>创建时间</b> 2014-3-1 <br>
  * <b>最后修改时间</b> 2014-5-30
- * 
- * @author kymjs (https://github.com/kymjs)
- * @version 1.0
+ *
+ * @author kymjs (http://www.kymjs.com)
+ * @version 2.25
  */
 public interface I_KJActivity {
-    /** 设置root界面 */
+
+    int DESTROY = 0;
+    int STOP = 2;
+    int PAUSE = 1;
+    int RESUME = 3;
+
+    /**
+     * 设置root界面
+     */
     void setRootView();
 
-    /** 初始化数据 */
+    /**
+     * 初始化数据
+     */
     void initData();
 
-    /** 在线程中初始化数据 */
+    /**
+     * 在线程中初始化数据
+     */
     void initDataFromThread();
 
-    /** 初始化控件 */
+    /**
+     * 初始化控件
+     */
     void initWidget();
 
-    /** 点击事件回调方法 */
+    /**
+     * 点击事件回调方法
+     */
     void widgetClick(View v);
 }
